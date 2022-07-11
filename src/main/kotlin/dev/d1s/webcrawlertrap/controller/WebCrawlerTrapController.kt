386127030d91250a61f49bb51790d47d49f7ab1f
d1snin <me@d1s.dev>
2022-07-11 14:15:08 +0300

    added `WebCrawlerTrapController`

--- src/main/kotlin/dev/d1s/webcrawlertrap/controller/WebCrawlerTrapController.kt
+++ src/main/kotlin/dev/d1s/webcrawlertrap/controller/WebCrawlerTrapController.kt
@@ -1,4 +1,38 @@
 package dev.d1s.webcrawlertrap.controller
 
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+import java.util.*
+import java.util.concurrent.ThreadLocalRandom
+
+@RestController
 class WebCrawlerTrapController {
+
+    private val formatter = HexFormat.of()
+
+    @GetMapping("/**", produces = [MediaType.TEXT_HTML_VALUE])
+    fun handle() = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+    </head>
+    <body>
+    <a href="${
+        run {
+            val buffer = ByteArray(64)
+
+            ThreadLocalRandom.current().nextBytes(buffer)
+
+            ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .replacePath("/${formatter.formatHex(buffer)}")
+                .build()
+                .toUriString()
+        }
+    }">:troll:</a>
+    </body>
+    </html> 
+    """.trimIndent()
 }
